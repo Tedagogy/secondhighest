d2fb6f5d20c5cf06a8e0870449a920b556631c82
ted balashov <theodore.balashov@revature.com>
2023-09-14 14:43:41 -0400

    lab

---
+++ src/main/java/Lab.java
@@ -0,0 +1,9 @@
+public class Lab {
+    /**
+     * @param arr an array of ints.
+     * @return the second highest value of arr.
+     */
+    public int getValue(int[] arr){
+        return 0;
+    }
+}
